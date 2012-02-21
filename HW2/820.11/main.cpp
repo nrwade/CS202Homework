375958f2423d4f311752c18cc49df6e477e088fb
nrwade <nrwade@alaska.edu>
2012-02-20 21:51:29 -0900

    Finished program

--- HW2/820.11/main.cpp
+++ HW2/820.11/main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 #include "NameArray.h";
 
 void addNumberToList(NameArray * n) {
-
 	float num;
 	int index;
 	cout << "Enter a number to add to the list: ";
@@ -21,11 +20,17 @@ void addNumberToList(NameArray * n) {
 }
 
 void getNumberFromList(NameArray * n) {
-	
+	int index;
+	cout << "Enter the index of the number you want: ";
+	cin >> index;
+	if (index-1 < 0) {
+		cout << "Only postive indices are allowed.\n";
+		return;
+	}
+	cout << ">> " << n->get(index-1) << endl;
 } 
 
 int main() {
-	
 	// Grab the length from the user
 	int length=-1;	
 	do {
@@ -46,11 +51,16 @@ int main() {
 	while (!quit) {
 		int menuOption;
 
+		cout << endl;
 		cout << "Menu Options: \n";
 		cout << "0. Quit Program\n";
-		cout << "1. Add a number to the list\n";
-		cout << "2. Get a number from the list\n";
-		cout << "3. Print list\n";
+		cout << "1. Print list\n";
+		cout << "2. Add a number to the list\n";
+		cout << "3. Get a number from the list\n";
+		cout << "4. Print Highest Number in the list\n";
+		cout << "5. Print Lowest Number in the list\n";
+		cout << "6. Print Average of list\n";
+		
 		cout << "---------------------------------\n";
 
 		cout << ": ";
@@ -62,15 +72,27 @@ int main() {
 				quit=!quit;
 				break;
 			case 1:
-				addNumberToList(n);
-				break;
-			case 2: 
-				getNumberFromList(n);
-				break;
-			case 3:
 				n->print();
 				break;
-			default: break;
+			case 2:
+				addNumberToList(n);
+				break;
+			case 3: 
+				getNumberFromList(n);
+				break;
+			case 4: 
+				cout << "max >> " << n->max() << endl;
+				break;
+			case 5: 
+				cout << "min >> " << n->min() << endl;
+				break;
+			case 6: 
+				cout << "avg >> " << n->avg() << endl;
+				break;
+				
+			default: 
+				cout << "That is not an option" << endl;
+				break;
 		}
 	}
 
