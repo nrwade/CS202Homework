d0d9f11be27893e99ccf6b0a19dff97cf2d200fc
nrwade <nrwade@alaska.edu>
2012-02-21 00:12:43 -0900

    added user input/output

--- HW2/890.7/main.cpp
+++ HW2/890.7/main.cpp
@@ -8,10 +8,44 @@ int main() {
 
 	Month m = Month();	
 
-	cout << m << endl;
+	cout << m;
 
-	cout << ": ";
+	cout << "Enter a month number (1-12): ";
 	cin >> m;
 	cout <<  m << endl;
+
+	cout << "Look at these fun months!" << endl;
+	m.setMonth(3);
+	cout << m;
+
+	string n = "feBrUary";
+	m.setMonth(n);
+	cout << m;
+
+	string n2 = "AUGUST";
+	m.setMonth(n2);
+	cout << m;
+
+	string n3 = "july";
+	m.setMonth(n3);
+
+	cout << m;
+
+	m++;
+	cout << m;
+
+	++m;
+	cout << m;
+
+	--m;
+	cout << m;
+	m--;
+	cout << m;
+
+	for (int i=1; i<=12; i++) {
+		m.setMonth(i);
+		cout << m;
+	}
+	
 	return 0;
 }
